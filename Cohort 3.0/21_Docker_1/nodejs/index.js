12dc9b9b97ca7a115e5142d071391b50ef5f9f09
Kunal-Rathore <kunalworkspace111@gmail.com>
2026-06-06 12:43:47 +0530

    learn:  -learned about volumes and network in docker

--- Cohort 3.0/21_Docker_1/nodejs/index.js
+++ Cohort 3.0/21_Docker_1/nodejs/index.js
@@ -1,6 +1,27 @@
 
 import express from "express";
+import { createClient } from "redis";
+
 const app = express();
+
+const redisClient = createClient({
+    socket: {
+        host: "redisDB",
+        port: 6379,
+    }
+});
+
+
+redisClient.on('error', (err) => {
+    console.log("redis client error:- ", err);
+})
+
+async function connectToRedis() {
+    await redisClient.connect();
+    console.log("Connected to redis successfully");
+}
+
+await connectToRedis();
 
 console.log("Hi from index.js");
 
